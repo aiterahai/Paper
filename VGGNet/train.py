b87413b6b058ef3c881d7a8dfa62c6b28285f33e
terra2007 <terra2007@naver.com>
2022-04-17 21:46:43 +0900

    [ADD] #2 train

--- VGGNet/train.py
+++ VGGNet/train.py
@@ -3,11 +3,12 @@ from dataloader import dataloader
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from tqdm import tqdm
 
 data_root = "../Datasets/CIFAR10"
 
 class_number = 10
-batch_size = 4
+batch_size = 256
 num_workers = 0
 learning_rate = 1e-4
 epoch = 100
@@ -22,3 +23,29 @@ trainloader, testloader = dataloader(batch_size=batch_size,
                                      data_root=data_root,
                                      num_workers=num_workers)
 
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(vggnet.parameters(), lr=learning_rate, momentum=0.9)
+
+def train(epochs, model):
+    for epoch_value in range(1, epochs + 1):
+        total_loss = 0
+        for img, data in tqdm(trainloader):
+            img, data = img.to(device), data.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(img)
+
+            loss = criterion(outputs, data)
+
+            loss.backward()
+
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        print(f"EPOCH : {epoch_value} LOSS : {total_loss / len(trainloader)}")
+
+train(epochs=epoch,
+      model=vggnet)
